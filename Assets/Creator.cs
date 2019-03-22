b1624b7b8fecbeeac1e197043b5e0521c6ec5d1e
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-03-22 16:08:35 -0300

    Add color variance

--- Assets/Creator.cs
+++ Assets/Creator.cs
@@ -7,6 +7,7 @@ public class Creator : MonoBehaviour
 {
     public GameObject Sand;
     private Color ActualColor;
+    public float variance = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,15 @@ public class Creator : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            Color color = ActualColor;
+            float tone = Mathf.Max(0, Mathf.Min(Random.Range(-variance, variance), 1));
+            color.r += tone;
+            color.g += tone;
+            color.b += tone;
+
             var mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             GameObject inst = Instantiate(Sand, new Vector3(mousePositionInWorld.x, mousePositionInWorld.y, 0), Quaternion.identity);
-            inst.GetComponent<SpriteRenderer>().color = ActualColor;
+            inst.GetComponent<SpriteRenderer>().color = color;
         }
     }
 
@@ -48,5 +55,10 @@ public class Creator : MonoBehaviour
     public void ChangeToRed()
     {
         ChangeColor(Color.red);
+    }
+
+    public void ChangeToDarkGreen()
+    {
+        ChangeColor(new Color32(0, 144, 0, 255));
     }
 }
