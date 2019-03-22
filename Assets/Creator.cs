f6705170b7a00eb7239544796486bcd49409b249
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-03-22 15:13:49 -0300

    Create initial version of the bottle

---
+++ Assets/Creator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Creator : MonoBehaviour
+{
+    public GameObject Sand;
+    private Color ActualColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActualColor = Color.yellow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject inst = Instantiate(Sand, new Vector3(mousePositionInWorld.x, mousePositionInWorld.y, 0), Quaternion.identity);
+            inst.GetComponent<SpriteRenderer>().color = ActualColor;
+        }
+    }
+
+    void ChangeColor(Color c)
+    {
+        ActualColor = c;
+    }
+
+    public void ChangeToYellow()
+    {
+        ChangeColor(Color.yellow);
+    }
+
+    public void ChangeToBlue()
+    {
+        ChangeColor(Color.blue);
+    }
+
+    public void ChangeToCyan()
+    {
+        ChangeColor(Color.cyan);
+    }
+}
---
+++ Assets/Sleeper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sleeper : MonoBehaviour
+{
+    private float CreatedFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatedFrame = Time.timeSinceLevelLoad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad - CreatedFrame >= 3 && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.01)
+        {
+            Destroy(GetComponent<Rigidbody2D>());
+            Destroy(this);
+        }
+    }
+}
